70649b953d460efb59c6027bb37f5d080db2ff28
hungspec <24100015@st.phenikaa-uni.edu.vn>
2025-08-14 15:45:36 +0700

    Delete QuanlyGiaoTrinh_springboot/complete/src/main/java/com/example/servingwebcontent/service directory

--- QuanlyGiaoTrinh_springboot/complete/src/main/java/com/example/servingwebcontent/service/Interfaces/LibraryDataInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.example.servingwebcontent.service.Interfaces;
-
-import java.util.ArrayList;
-
-public interface LibraryDataInitializer {
-    ArrayList<Object> initializeData(Object obj);
-}
